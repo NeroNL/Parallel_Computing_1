7176f955c3b41d9d2de8269385af43ed797dd669
Scott Baden <sbaden@ucsd.edu>
2015-04-05 11:53:33 -0700

    slight cleanup

--- pmdb.cpp
+++ pmdb.cpp
@@ -1,6 +1,12 @@
-#include <pthread.h>
+#include <iostream>
+#include <assert.h>
+#include <cstdlib>
+#include <thread>
+using namespace std;
 
-// Implemented for you in smdb.h
+
+// Point computatoin (serial)
+// Implemented in smdb.h
 int ComputeMandelbrotPoint(int x, int y, int dimX, int dimY);
 
 // Implement the thread function which will be called by the spawned threads
@@ -10,7 +16,7 @@ int ComputeMandelbrotPoint(int x, int y, int dimX, int dimY);
 // It will spawn and join threads, calling on your thread function
 // to perform the former task
 //
-void Mandelbrot_pthreads(int** pts, int dimX, int dimY, int numThreads, int chunkSize) 
+void Mandelbrot_thread(int** pts, int dimX, int dimY, int numThreads, int chunkSize) 
 {
   //////////// add your code here //////////////////////////////////
 }
